37b00e1608a98d9c64c684335e9d215e40d54154
Well Silver <wellsilver2016@gmail.com>
2022-05-05 19:15:13 +0000

    readme and further user preparations

---
+++ readme.md
@@ -0,0 +1,9 @@
+# Branch
+Branch is a cool place where you can share mods and later plugins/mods,
+
+## inspiration
+
+Where did I get inspiration? I took one look at hangars source code and collapsed inside whondering why people who use java are so bad at making things readable, I could do one thing in 3 lines that it took them 2 files of 1000+ lines >:( this also applies to their website papermc.io aaaa
+
+## Why should I use branch
+You got me there, this is pretty bad
--- src/main.py
+++ src/main.py
@@ -1,34 +1,68 @@
-from flask import Flask,request,make_response,send_file
+from flask import Flask,request,make_response,send_file,redirect
 import api
 app = Flask('app')
-
+"""draft for profile------
+<button class="button" style="display:inline;float:left;">Profile</button>
+<button class="button" style="display:inline;float:right;">Create</button>
+<h2 style="text-align:center;">Branch</h2>
+<hr noshade/>
+<div class="Profile">
+	<div class="profile-top">
+    	<p style="text-decoration:bold;">{username}</p>
+        <p>{shortdesc}</p>
+    </div>
+    <hr/>
+    <p>{longdesc}</p>
+    <hr/>
+    <div class="mod_embed">
+    	<p class="ting">{mod1}</p>
+    	<p class="ting">{mod1short}</p>
+    </div>
+    <div class="mod_embed">
+    	<p class="ting">{mod2}</p>
+        <p class="ting">{mod2short}</p>
+    </div>
+    <div class="mod_embed">
+    	<p class="ting">{mod3}</p>
+        <p class="ting">{mod3short}</p>
+	</div>
+</div>"""
 @app.route('/')
 def index():
   cookie = request.cookies.get('Token')
   if api.cookie.valid(cookie):
     return '''
-<button style="display:inline;float:left;">Profile</button>
-<button style="display:inline;float:right;">Create</button>
+    <link rel="stylesheet" href="api/style">
+<button class="button" style="display:inline;float:left;">Profile</button>
+<button class="button" style="display:inline;float:right;">Create</button>
 <h2 style="text-align:center;">Branch</h2>
 <hr noshade/>'''
   else:
     return '''
-<button style="display:inline;float:left;">Sign in</button>
-<button style="display:inline;float:right">Sign up</button>
+<link rel="stylesheet" href="api/style">
+<button class="button" style="display:inline;float:left;">Sign in</button>
+<button class="button" style="display:inline;float:right">Sign up</button>
 <h2 style="text-align:center;">Branch</h2>
 <hr noshade/>'''
 ######################
 #resp = make_response(render_template())
 #resp.set_cookie('userID', user)
+#<link rel="stylesheet" href="api/style">
+@app.route('/login')
+def login():
+  return 'cool'
 @app.route('/profile')
 @app.route('/profile/<ID>')
 def profiles(ID=None):
   if ID==None:
     cookie = request.cookies.get('Token')
+    if cookie == None:
+      return redirect("/login",302)
 
-
-@app.route('/api/style')
-def getstylebecausecool():
-  return send_file("html/index.css")
-    
+@app.route('/api/<s>')
+def getstylebecausecool(s):
+  if s=="style":
+    return send_file("src/html/index.css")
+  return send_file("src/html/s")
+  
 app.run(host='0.0.0.0', port=80)
