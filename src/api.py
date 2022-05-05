70fc8c757001029debc26bc93de47c64c7cdccd9
Well Silver <wellsilver2016@gmail.com>
2022-05-05 14:24:38 +0000

    changes

--- src/api.py
+++ src/api.py
@@ -1,4 +1,28 @@
-def makeuser(username,password,shortdesc):
+import secrets
+cookies=[]
+# ^ change to file
+class cookie:
+  def valid(cookie):
+    f=False
+    global cookies
+    for i in cookies:
+      if i == cookie:
+        f=True
+        break
+    if f ==True:
+      return True
+    else:
+      return False
+  def make(userID):
     pass
-def changeuserdesc(type,description):
+class user:
+  def get(ID):
+    try:
+      file=open(f"store/user/{ID}.toml")
+    except:
+      return None
+    
+  def create(user,passw):
+    pass
+  def change(mode):
     pass
--- src/main.py
+++ src/main.py
@@ -1,9 +1,15 @@
-from flask import Flask
+from flask import Flask,render_template,request
 import api
 app = Flask('app')
 
 @app.route('/')
 def index():
-  return 'Hello, World!'
+  cookie = request.cookies.get('Token')
+  if api.cookie.valid(cookie):
+    return '''
+<button style="display:inline;float:left;"></button>
+<button style="display:inline;float:right;">{buttonr}</button>
+<h2 style="text-align:center;">Branch</h2>
+<hr noshade/>'''
 
 app.run(host='0.0.0.0', port=80)
